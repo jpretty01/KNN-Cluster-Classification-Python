b4c42e0b6d5c7fddc2e4e27bddf1295f134a2dbe
jpretty01 <jeremypretty10@gmail.com>
2021-12-12 14:47:17 -0600

    Code
    Code

---
+++ Jeremy Pretty CSC 525 Module 2 Option 1.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 12 13:58:15 2021
+
+@author: jpret
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#path = "(https://gist.githubusercontent.com/gurchetan1000/ec90a0a8004927e57c24b20a6f8c8d35/raw/fcd83b35021a4c1d7f1f1d5dc83c07c8ffc0d3e2/iris.csv"
+#path = "iris.csv"
+headernames = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'Class']
+
+#dataset = pd.read_csv(path, names=headernames)
+dataset = pd.read_csv("iris.csv", names=headernames)
+dataset.head()
+
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 4].values
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.40)
+
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+scaler.fit(X_train)
+X_train = scaler.transform(X_train)
+X_test = scaler.transform(X_test)
+
+from sklearn.neighbors import KNeighborsClassifier
+classifier = KNeighborsClassifier(n_neighbors=8)
+classifier.fit(X_train, y_train)
+
+y_pred = classifier.predict(X_test)
+
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+result = confusion_matrix(y_test, y_pred)
+print("Confusion Matrix:")
+print(result)
+result1 = classification_report(y_test, y_pred)
+print("Classification Report:",)
+print (result1)
+result2 = accuracy_score(y_test,y_pred)
+print("Accuracy:",result2)
